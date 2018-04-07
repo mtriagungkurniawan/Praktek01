a80f4869b120426a313c1c0600d977f97c8bf113
M.Triagung Kurniawan <m.triagungkurniawan18@gamil.com>
2018-04-07 08:34:33 +0700

    Mengisi Atribut

--- src/praktek01/DosenAksi.java
+++ src/praktek01/DosenAksi.java
@@ -15,6 +15,24 @@ public class DosenAksi {
         Dosen SI = new Dosen();
         Dosen MT = new Dosen();
         
+        TI.ND="Kambrianor";
+        TI.NIP="16612345342523";
+        TI.Alamat="Pleihari";
+        TI.Matkul="Teknik Informatika";
+        TI.LM="4 Tahun";
+        
+        SI.ND="Hafiz";
+        SI.NIP="1661234534444";
+        SI.Alamat="Amuntai";
+        SI.Matkul="Sistem Informasi";
+        SI.LM="15 Tahun";
+        
+        MT.ND="Kambrianor";
+        MT.NIP="16612345342523";
+        MT.Alamat="Pleihari";
+        MT.Matkul="Teknik Informatika";
+        MT.LM="4 Tahun";
+        
     
 }
 }
